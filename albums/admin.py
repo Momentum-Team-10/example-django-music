9479fbd644b7926d4ace73c42b8848f94dfb1249
Amy Gori <amydgori@gmail.com>
2021-11-08 11:42:32 -0500

    Add custom user model and start adding django-registration-redux

--- albums/admin.py
+++ albums/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Album
+from .models import Album, User
+from django.contrib.auth.admin import UserAdmin
+
 
 # Register your models here.
 admin.site.register(Album)
-
-
+admin.site.register(User, UserAdmin)
