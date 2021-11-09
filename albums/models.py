18c30c3e707ee26c7c6618114a9de18d13b06669
Amy Gori <amydgori@gmail.com>
2021-11-09 17:24:11 -0500

    Add Artist model and relationship with Album

--- albums/models.py
+++ albums/models.py
@@ -8,13 +8,36 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+
+class Genre(models.Model):
+    name = models.CharField(max_length=75)
+    slug = models.SlugField(max_length=75, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"<Genre name={self.name}>"
+
+class Artist(models.Model):
+    name = models.CharField(max_length=255)
+    birthplace = models.CharField(max_length=255)
+    birthdate = models.DateField(null=True, blank=True)
+
+    def __repr__(self):
+        return f"<Artist name={self.name}>"
+
+    def __str__(self):
+        return self.name
+
 class Album(models.Model):
     title = models.CharField(max_length=255)
-    artist_name = models.CharField(max_length=255)
+    artist = models.ForeignKey(Artist, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __repr__(self):
-        return f"<Album title={self.title} artist_name={self.artist_name}>"
+        return f"<Album title={self.title} >"
 
     def __str__(self):
         return self.title
+
