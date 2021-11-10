4e6c82b40a414a059a65d9868e5d9faec37e96f2
Amy Gori <amydgori@gmail.com>
2021-11-10 13:13:49 -0500

    Add M2M relationship

--- albums/admin.py
+++ albums/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Album, Artist, User
+from .models import Album, Artist, Genre, User
 from django.contrib.auth.admin import UserAdmin
 
 
 # Register your models here.
 admin.site.register(Album)
 admin.site.register(Artist)
+admin.site.register(Genre)
 admin.site.register(User, UserAdmin)
--- albums/models.py
+++ albums/models.py
@@ -34,10 +34,10 @@ class Album(models.Model):
     title = models.CharField(max_length=255)
     artist = models.ForeignKey(Artist, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    genres = models.ManyToManyField('Genre', related_name="albums")
 
     def __repr__(self):
         return f"<Album title={self.title} >"
 
     def __str__(self):
         return self.title
-
